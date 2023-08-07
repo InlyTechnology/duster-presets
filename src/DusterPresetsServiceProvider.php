f83b97f01bf94b2ce1ba9d7c9b03fc3388d5b12d
Joel-Jensen <Joel-Jensen@users.noreply.github.com>
2023-08-07 09:37:15 +0000

    Fix styling

--- src/DusterPresetsServiceProvider.php
+++ src/DusterPresetsServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace Inly\DusterPresets;
 
-use Inly\DusterPresets\Commands\DusterPresetsCommand;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 
