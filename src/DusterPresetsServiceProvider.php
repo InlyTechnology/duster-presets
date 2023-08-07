a2fd5ea8454cfb8af35821bf11b5133f61702586
Joel-Jensen <info@joeljensen.se>
2023-08-07 11:43:55 +0200

    tlint.json

--- src/DusterPresetsServiceProvider.php
+++ src/DusterPresetsServiceProvider.php
@@ -21,7 +21,8 @@ class DusterPresetsServiceProvider extends PackageServiceProvider
     public function bootingPackage()
     {
         $this->publishes([
-            $this->package->basePath('/pint.json') => base_path(),
+            $this->package->basePath('/pint.json') => base_path("pint.json"),
+            $this->package->basePath('/tlint.json') => base_path("tlint.json"),
         ], "laravel-assets");
     }
 }
