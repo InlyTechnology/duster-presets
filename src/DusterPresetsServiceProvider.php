d2d09f4288a4af27c06ef9d36c4dba2658f6300a
Joel-Jensen <info@joeljensen.se>
2023-08-08 15:37:47 +0200

    Skip tag, use provider

--- src/DusterPresetsServiceProvider.php
+++ src/DusterPresetsServiceProvider.php
@@ -22,6 +22,6 @@ class DusterPresetsServiceProvider extends PackageServiceProvider
     {
         $this->publishes([
             $this->package->basePath('/../stubs') => base_path('/'),
-        ], "{$this->package->shortName()}-stubs");
+        ]);
     }
 }
